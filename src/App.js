05b8838d01c22f9a105821c5674db9df1a3199a0
Aarjit <poudelaarjit@gmail.com>
2023-02-09 18:41:05 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,101 @@
-import Button from "./Button";
-const App = () =>{
-    return <div>
-    <h1>Recently Used Websites</h1>
-    <h3>Which website do you want to open?</h3>
-    <Button name="Google" link="https://google.com"></Button>
-    <Button name="Youtube"  link="https://youtube.com"></Button>
-    <Button name="Gmail" link="https://gmail.com"></Button> <br></br>
-    <Button name="LinkedIn" link="https://linkedin.com"></Button> 
-    <Button name="Github"link="https://github.com"></Button>
+import React, { useState } from "react";
+import "./style.css";
+
+const App = () => {
+  const [columns, setColumns] = useState([
+    { title: "Column A", childs: ["A1", "A2", "A3"] },
+    { title: "Column B", childs: ["B1", "B2", "B3"] },
+    { title: "Column C", childs: ["C1", "C2", "C3"] },
+  ]);
+
+  const addColumn = () => {
+    setColumns([
+      ...columns,
+      { title: `Column ${columns.length + 1}`, childs: [] },
+    ]);
+  };
+
+  const deleteColumn = (index) => {
+    setColumns(columns.filter((_, i) => i !== index));
+  };
+
+  const onDragStart = (event, columnIndex, itemIndex) => {
+    event.dataTransfer.setData("columnIndex", columnIndex);
+    event.dataTransfer.setData("itemIndex", itemIndex);
+  };
+
+  const onColumnDragStart = (event, columnIndex) => {
+    event.dataTransfer.setData("columnIndex", columnIndex);
+    event.dataTransfer.setData("isColumn", "true");
+  };
+
+  const onDrop = (event, toColumnIndex) => {
+    const fromColumnIndex = parseInt(
+      event.dataTransfer.getData("columnIndex")
+    );
+    const isColumn = event.dataTransfer.getData("isColumn") === "true";
+    if (isColumn) {
+      const newColumns = [...columns];
+      const [column] = newColumns.splice(fromColumnIndex, 1);
+      newColumns.splice(toColumnIndex, 0, column);
+      setColumns(newColumns);
+    } else {
+      const itemIndex = parseInt(event.dataTransfer.getData("itemIndex"));
+      const newColumns = [...columns];
+      const [item] = newColumns[fromColumnIndex].childs.splice(itemIndex, 1);
+      newColumns[toColumnIndex].childs.push(item);
+      setColumns(newColumns);
+    }
+  };
+
+  const onDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="container">
+      {columns.map((column, index) => (
+        <div
+          className="column"
+          key={index}
+          onDrop={(event) => onDrop(event, index)}
+          onDragOver={onDragOver}
+        >
+          <div
+            className="column-header"
+            draggable
+            onDragStart={(event) => onColumnDragStart(event, index)}
+          >
+            {column.title}
+            <span
+              className="delete-icon"
+              onClick={() => deleteColumn(index)}
+            >
+              &#10005;
+            </span>
+          </div>
+          
+          <div className="column-body">
+            {column.childs.map((child, i) => (
+              <div
+                className="item"
+                key={i}
+                draggable
+                onDragStart={(event) => onDragStart(event, index, i)}
+         
+              >
+                {child}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+      <button className="add-column" onClick={addColumn}>
+        Add Column
+      </button>
     </div>
-}
+  );
+};
 
 export default App;
+
--- src/Button.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const Button = ({name, link}) =>{
-    return <a href={link} target="_blank"><button className="button">{name}</button></a>
-}
-export default Button;
